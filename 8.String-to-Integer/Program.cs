26843b0841fe362496f5f29e8d5639ea52ca8a63
Matt Neill <mattneill000@hotmail.com>
2025-01-07 14:50:32 +1000

    solutions 8-10

---
+++ 8.String-to-Integer/Program.cs
@@ -0,0 +1,57 @@
+﻿class Program
+{
+    static void Main()
+    {
+        string input = "   ";
+        Console.WriteLine($"Extracted Number: {ExtractInteger(input)}");
+    }
+
+    static int ExtractInteger(string input)
+    {
+        bool isNegative = false;
+        long extractedNumber = 0;
+
+        const int INT_MIN = -2147483648;
+        const int INT_MAX = 2147483647;
+
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            return 0;
+        }
+
+        input = input.TrimStart();
+
+        if (input[0] == '-' || input[0] == '+')
+        {
+            isNegative = input[0] == '-';
+            input = input.Substring(1);
+        }
+        foreach (char c in input)
+        {
+            if (char.IsDigit(c))
+            {
+                extractedNumber = extractedNumber * 10 + (c - '0');
+                if (isNegative && -extractedNumber < INT_MIN)
+                {
+                    return INT_MIN;
+                }
+                else if (!isNegative && extractedNumber > INT_MAX)
+                {
+                    return INT_MAX;
+                }
+            }
+            else
+            {
+                break;
+            }
+        }
+        if (isNegative)
+        {
+            extractedNumber = -extractedNumber;
+        }
+        if (extractedNumber < INT_MIN) return INT_MIN;
+        if (extractedNumber > INT_MAX) return INT_MAX;
+
+        return (int)extractedNumber;
+    }
+}
---
+++ 9.Palindrome/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Solution {
+    static void Main() {
+        int input = 121;
+        Console.WriteLine(IsPalindrome(input));
+
+        input = -121;
+        Console.WriteLine(IsPalindrome(input));
+
+        input = 10;
+        Console.WriteLine(IsPalindrome(input));       
+    }
+
+    static bool IsPalindrome(int number){
+        if (number < 0 || (number % 10 == 0 && number != 0)) {
+            return false;
+        }
+
+        int reversedNumber = 0;
+        int original = number;
+
+        while (number > 0) {
+            reversedNumber = reversedNumber * 10 + number % 10;
+            number /= 10;
+        }
+
+        return original == reversedNumber;
+    }
+}
